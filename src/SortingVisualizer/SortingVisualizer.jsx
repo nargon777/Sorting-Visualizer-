ca7a42a598434c42f7c4c94434addd1c8cd9dfb7
nargon777 <nandrews11222@gmail.com>
2023-05-10 13:32:26 -0700

    -

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -24,7 +24,7 @@ export default class SortingVisualizer extends React.Component {
     resetArray() {
         const array = [];
         for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
-            array.push(randomIntFromInterval(5, 700));
+            array.push(randomIntFromInterval(5, 730));
         }
         this.setState({array});
     }
